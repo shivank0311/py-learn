722e368ac94a71b79d1ceb914ba17462660c4c06
shivank0311 <shivank0311@gmail.com>
2022-07-14 23:32:21 +0530

    other syntax file

---
+++ syntax file/conditional.py
@@ -0,0 +1,12 @@
+def main():
+     a , b = 2 , 2
+     if a < b:
+        print("a is smaller than b")
+
+     elif  a > b:
+        print("a is greater than b")
+
+     else:   
+        print("a is equal to b")
+
+if __name__ == "__main__": main()     
---
+++ syntax file/whitespace.py
@@ -0,0 +1,6 @@
+def main():
+    print("this is my syntax.py file")
+print("this is another line")
+
+if __name__ == "__main__" :main() 
+     
