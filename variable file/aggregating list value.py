6361ac316500ad4429f95b777ea9ba197d7a43e7
Shivank Singh <Shivank0311@gmail.com>
2022-08-19 20:44:16 +0530

    new file addition

---
+++ variable file/aggregating list value.py
@@ -0,0 +1,6 @@
+def main():
+    x = [1,2,3,4]
+    x.append(5)
+    print(type(x),x)
+
+if __name__=="__main__":main()    
---
+++ variable file/aggregating value.py
@@ -0,0 +1,5 @@
+def main():
+    x = (1,2,3,4)
+    print (type(x),x)
+
+if __name__=="__main__":main()    
