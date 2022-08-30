ca14ca1509a27dc74faf94783e5a064a7e18cd24
Shivank Singh <Shivank0311@gmail.com>
2022-08-30 22:54:56 +0530

    learning loops

---
+++ Loops/break &continue.py
@@ -0,0 +1,7 @@
+def main():
+    s="this is string"
+    for c in s:
+        if c=="s":continue
+        print(c,end="")
+
+if __name__=="__main__" :main()
---
+++ Loops/enumerate.py
@@ -0,0 +1,6 @@
+def main():
+    s="this is string"
+    for i, c in enumerate(s):
+        if c=="i": print("index {} is i".format(i))
+
+if __name__=="__main__" :main()       
